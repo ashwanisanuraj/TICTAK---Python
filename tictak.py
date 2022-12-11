baaee137807b4f1c06f407e5a373b6c6dfea9b49
ashwanisanuraj <33251838+ashwanisanuraj@users.noreply.github.com>
2022-12-11 12:36:53 +0530

    Update tictak.py

--- tictak.py
+++ tictak.py
@@ -1,4 +1,6 @@
 #creating the table
+import time
+import sys
 
 board=['-','-','-','-','-','-','-','-','-']
 
@@ -40,14 +42,41 @@ def check(board):
     else:
         return False
 
-def inp(board):
-    x = int(input("Enter the position: "))
-    # return x
-    if board [x-1]!='-':
-        print('INVALID POSITION,\nEnter Again!!')
-        return inp(board)
-    else:
-        return x
+# def inp(board):
+#     x = int(input("Enter the position: "))
+#     # return x
+#     if board [x-1]!='-':
+#         print('INVALID POSITION,\nEnter Again!!')
+#         return inp(board)
+#     else:
+#         return x
+
+def animate():
+        sys.stdout.write('.')
+        time.sleep(0.8)
+        sys.stdout.write('.')
+        time.sleep(0.8)
+        sys.stdout.write('.')
+        time.sleep(0.8)
+
+def gameover():
+    sys.stdout.write('G')
+    time.sleep(0.3)
+    sys.stdout.write('A')
+    time.sleep(0.3)
+    sys.stdout.write('M')
+    time.sleep(0.3)
+    sys.stdout.write('E')
+    time.sleep(0.25)
+    sys.stdout.write(' ')
+    time.sleep(0.25)
+    sys.stdout.write('O')
+    time.sleep(0.3)
+    sys.stdout.write('V')
+    time.sleep(0.3)
+    sys.stdout.write('E')
+    time.sleep(0.3)
+    sys.stdout.write('R')
 
 player1 = input('Enter Name of Player 1: ')
 player2 = input('Enter Name of Player 2: ')
@@ -55,21 +84,26 @@ display()
 
 for i in range(9):
     if i%2 == 0:
-        x = inp(board)
+        # x = inp(board)
+        x = int(input(str(player1)+', its your turn: '))
         board[x-1] = 'x'
         display()
         print(str(player1)+' pressed '+ str(x))
         if check(board):
-            print(str(player1)+' Wins🎉')
+            animate()
+            print('\n'+str(player1)+' Wins🎉')
             break
         
     else:
-        x = inp(board)
+        # x = inp(board)
+        x = int(input(str(player2)+', its your turn: '))
         board[x-1] = 'o'
         display()
         print(str(player2)+' pressed '+ str(x))
         if check(board):
-            print(str(player2)+' Wins🎉')
+            animate()
+            print('\n'+str(player2)+' Wins🎉')
             break
-        
-print("GAME OVER!!")
+
+gameover()     
+
